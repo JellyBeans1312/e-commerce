d3784264ac10451b90c3581dd4a6b339473456c2
Aidan McKay <aidanmckay2000@gmail.com>
2024-09-27 13:52:48 -0500

    Utilize hono to create post route for stores api

--- cms-dashboard/app/api/[[...route]]/route.ts
+++ cms-dashboard/app/api/[[...route]]/route.ts
@@ -1,15 +1,18 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
+import stores  from './stores'
+
 export const runtime = 'nodejs'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
-  return c.json({
-    message: 'Hello Next.js!',
-  })
-})
+const routes = app
+    .route('/stores', stores)
 
 export const GET = handle(app)
 export const POST = handle(app)
+export const PATCH = handle(app)
+export const DELETE = handle(app)
+
+export type AppType = typeof routes;
